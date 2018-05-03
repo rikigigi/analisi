1f8ec375835b640221bae82e73eedf134b7b5cf2
Riccardo Bertossa <rbertoss@sissa.it>
2018-05-03 14:22:13 +0200

    sistemato un errore nello script di conversione da lammps (non binario)

--- lammps2analisi.py
+++ lammps2analisi.py
@@ -4,14 +4,19 @@ import numpy as np
 import sys
 
 
-def read_write_xyz(infile,outfile,cols_pos=[3,4,5],cols_vel=None):
+def read_write_xyz(infile,outfile,cols_pos=[3,4,5],cols_vel=None,col_id=0,col_type=1):
     """
     """
    
     in_file = open(infile, "r") 
     out_file = open(outfile, "w")
     #out_file.write("This Text is going to out file\nLook at it and see\n")
+    cont=0
     while True:
+        if (cont%200==0):
+            sys.stdout.write('.')
+            sys.stdout.flush()
+        cont+=1
         line=in_file.readline()
         if not line: break
         line=in_file.readline()
@@ -34,8 +39,7 @@ def read_write_xyz(infile,outfile,cols_pos=[3,4,5],cols_vel=None):
         for iatom in range(int(natoms)):
              line = in_file.readline()
              line_s=line.split()
-             print line
-             line_o=line_s[0]
+             line_o=line_s[col_id]+line_s[col_type]
              for col in cols_pos:
                  line_o += " {}".format(line_s[col])
              if cols_vel != None:
@@ -45,19 +49,25 @@ def read_write_xyz(infile,outfile,cols_pos=[3,4,5],cols_vel=None):
                  line_o+=" 0 0 0"
              out_file.write(line_o+'\n')
     out_file.close()
+    print "\n {} total.".format(cont)
     return
 
-if len(sys.argv)<3:
-    print "Uso: {} [prefix_file_in] [file_out] [col_pos_x col_pos_y col_pos_z] [col_vel_x col_vel_y col_vel_z] ...".format(sys.argv[0])
+if len(sys.argv)<5:
+    print "Uso: {} [prefix_file_in] [file_out] [col_id] [col_type] [col_pos_x col_pos_y col_pos_z] [col_vel_x col_vel_y col_vel_z] ...".format(sys.argv[0])
     exit(-1)
 
 col_pos=[3,4,5]
 col_vel=None
-if len(sys.argv) == 6 or len(sys.argv) == 9:
-    col_pos=[int(sys.argv[3]),int(sys.argv[4]),int(sys.argv[5])]
-if len(sys.argv) == 9 :
-    col_vel=[int(sys.argv[6]),int(sys.argv[7]),int(sys.argv[8])]
+if len(sys.argv) == 8 or len(sys.argv) == 11:
+    col_pos=[int(sys.argv[5]),int(sys.argv[6]),int(sys.argv[7])]
+if len(sys.argv) == 11 :
+    col_vel=[int(sys.argv[8]),int(sys.argv[9]),int(sys.argv[10])]
 
-read_write_xyz(sys.argv[1],sys.argv[2],col_pos,col_vel)
+print "velocities: ", col_pos
+print "positions: ", col_vel
+print "id: ", int(argv[3])
+print "type: ",int(argv[4])
+
+read_write_xyz(sys.argv[1],sys.argv[2],col_pos,col_vel,int(argv[3]),int(argv[4]))
 
 
