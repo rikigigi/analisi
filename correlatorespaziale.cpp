12c3653519dce163912692ee6e6e62767ffe1a50
Riccardo Bertossa <rbertoss@sissa.it>
2018-12-04 17:18:33 +0100

    modificato il correlatore delle velocita' aggiunti 2 file uno chiamato correlatorespaziale.cpp_davide e correlatorespaziale.cpp_riccardo il primo e' una versione in cui i dati dovrebbero essere messi in memoria seguendo kz: gli indici che variano sono in ordine i j kx ky kz : dove i<tipi_atomo ; j<=i ; ky,kx<nk ; kz<nk/2+1 quindi in sfac[l] muovere l=l+1 vuol dire muovere kz=kz+1
    il secondo correlatorespaziale.cpp_riccardo e' quello che abbiamo fatto io e riccardo
    in memoria sono contigue le componenti con diversi indice di tipi
    nell esempio di prima
    muovere l=l+1 vuol dire muovere j=j+1

--- correlatorespaziale.cpp
+++ correlatorespaziale.cpp
@@ -33,9 +33,10 @@ void CorrelatoreSpaziale::reset(const unsigned int numeroTimestepsPerBlocco) {
         sfac = (fftw_complex *) fftw_malloc(sizeof(fftw_complex)*3*nk*nk*(nk/2+1)*tipi_atomi*(tipi_atomi+1)/2);
         //lista = new double[nk*nk*(nk/2+1)*tipi_atomi*(tipi_atomi+1)/2*3];
         lista = new double[nk*nk*nk*tipi_atomi*(tipi_atomi+1)/2*3];
-        //lista = (double *) fftw_malloc(sizeof(double)*nk*nk*nk*tipi_atomi*(tipi_atomi+1)/2*3);
+       // lista = (double *) fftw_malloc(sizeof(double)*nk*nk*nk*tipi_atomi*(tipi_atomi+1)/2*3);
     }
     ntimesteps=numeroTimestepsPerBlocco;
+    std::cerr<< "Sto usando il CorrSpaziale Davide"<<std::endl;
 }
 
 
@@ -58,7 +59,8 @@ void CorrelatoreSpaziale::calcola(unsigned int primo) {
 
     //size = nk*nk*(nk/2+1)*tipi_atomi*(tipi_atomi+1)/2;
     size = nk*nk*nk*tipi_atomi*(tipi_atomi+1)/2;
-    int size_half=nk*nk*(nk/2+1)*tipi_atomi*(tipi_atomi+1)/2;
+    //int size_half=nk*nk*(nk/2+1)*tipi_atomi*(tipi_atomi+1)/2;
+    int size_half=nk*nk*(nk/2+1)+tipi_atomi*(tipi_atomi+1)/2;
     int itime = 0;
 
     for (unsigned int i=0;i<3*nk*nk*(nk/2+1)*tipi_atomi*(tipi_atomi+1)/2;i++) {
@@ -66,21 +68,22 @@ void CorrelatoreSpaziale::calcola(unsigned int primo) {
         sfac[i][1]=0.0;
     }
     fftw_complex * sfac_t=(fftw_complex *) fftw_malloc(sizeof(fftw_complex) * tipi_atomi*3);
-    int n[]={nk,nk,nk};
-    //int n[]={nk,nk,nk/2+1};
+    //int n[]={nk,nk,nk};
+    //int n[]={1,1,1};
+    int n[]={nk,nk,nk/2+1};
     fftw3 = fftw_plan_many_dft_c2r(3, // rango della trasformata (3D)
                                    n, // lunghezza di ciascuna trasformata
-                                    tipi_atomi*(tipi_atomi+1)/2, // numero di trasformate
+                                   tipi_atomi*(tipi_atomi+1)/2, // numero di trasformate
                                    /*  ****** input ******  */
                                    sfac,//traiettoria->velocita_inizio(), // puntatore ai dati
                                    NULL, // i dati sono tutti compatti, non fanno parte di array più grandi
-                                   (tipi_atomi+1)*tipi_atomi/2, // la distanza fra due dati successivi
-                                   1, // la distanza fra due serie di dati adiacenti
+                                   1, // la distanza fra due dati successivi
+                                   nk*nk*(nk/2+1), // la distanza fra due serie di dati adiacenti
                                    /*  ****** output ******  */
                                    lista, // puntatore all'array di output
                                    NULL,
-                                   (tipi_atomi+1)*tipi_atomi/2, // la distanza fra due dati successivi
-                                   1, // la distanza fra due serie di dati adiacenti
+                                   1, // la distanza fra due dati successivi
+                                   nk*nk*(nk/2+1), // la distanza fra due serie di dati adiacenti
                                    FFTW_PRESERVE_INPUT
                                    );
 
@@ -89,38 +92,78 @@ void CorrelatoreSpaziale::calcola(unsigned int primo) {
         double * box=t->scatola(itimestep);
         //ciclo su kx,ky e kz. kz va da 0 a kz/2+1
         double k[3]={0.0};
-        for (int kx=0;kx<nk;kx++){
-            unsigned int ikx=kx*nk*(nk/2+1)*(tipi_atomi+1)*tipi_atomi/2;
-            k[0]=PI/(box[1]-box[0])*kx;
-            for (int ky=0;ky<nk;ky++) {
-                unsigned int iky=ky*(nk/2+1)*(tipi_atomi+1)*tipi_atomi/2;
-                k[1]=PI/(box[1]-box[0])*ky;
-                for (int kz=0;kz<nk/2+1;kz++) {
-                    for (unsigned int i=0;i<tipi_atomi*3;i++) {
-                        sfac_t[i][0]=0.0;
-                        sfac_t[i][1]=0.0;
-                    }
-                    unsigned int ik=ikx+iky+kz*(tipi_atomi+1)*tipi_atomi/2;
-                    k[2]=PI/(box[1]-box[0])*ky;
-                  //  sfac_t[0][0]=0.1 ;/////// TOGLIERE
-                    s_fac_k(k,itimestep,sfac_t);
-                    unsigned int m=0;
-                    for (unsigned int i=0;i<tipi_atomi;i++)
-                        for (unsigned int j=0;j<=i;j++){
-                            //unsigned int m = (tipi_atomi+1)*tipi_atomi/2 - (i+1)*(i+2)/2 + j;
+        unsigned int ii_tipi=0;
+        for (unsigned int i=0;i<tipi_atomi;i++){
+            for (unsigned int j=0;j<=i;j++){
+                int ii_tipi_matrix=ii_tipi*nk*nk*(nk/2+1);
+                for (int kx=0;kx<nk;kx++){
+                    unsigned int ikx=ii_tipi_matrix+kx*nk*(nk/2+1);  
+                    k[0]=PI/(box[1]-box[0])*kx;
+                    for (int ky=0;ky<nk;ky++) {
+                        unsigned int iky=ikx+ky*(nk/2+1);  
+                        k[1]=PI/(box[1]-box[0])*ky;
+                        for (int kz=0;kz<nk/2+1;kz++) {
+                            for (unsigned int i=0;i<tipi_atomi*3;i++) {
+                                sfac_t[i][0]=0.0;
+                                sfac_t[i][1]=0.0;
+                            }
+                            unsigned int ik=iky+kz;
+                            //k[2]=PI/(box[1]-box[0])*ky; ///// MERDA IL COPIA/INCOLLA
+                            k[2]=PI/(box[1]-box[0])*kz;
+                          //  sfac_t[0][0]=0.1 ;/////// TOGLIERE
+                            s_fac_k(k,itimestep,sfac_t);
 
-                            sfac[ik+m][0]+=       ( sfac_t[i*3][0]*  sfac_t[j*3][0]  -sfac_t[i*3][1]*  sfac_t[j*3][1] -sfac[ik+m][0])/(itime);
-                            sfac[ik+m][1]+=       (-sfac_t[i*3][0]*  sfac_t[j*3][1] + sfac_t[i*3][1]*  sfac_t[j*3][0] -sfac[ik+m][1])/(itime);
-                            sfac[size_half  +ik+m][0]+=  ( sfac_t[i*3+1][0]*sfac_t[j*3+1][0]-sfac_t[i*3+1][1]*sfac_t[j*3+1][1]  - sfac[  size_half+ik+m][0])/(itime);
-                            sfac[size_half  +ik+m][1]+=  (-sfac_t[i*3+1][0]*sfac_t[j*3+1][1]+sfac_t[i*3+1][1]*sfac_t[j*3+1][0] -  sfac[  size_half+ik+m][1])/(itime);
-                            sfac[2*size_half+ik+m][0]+=( sfac_t[i*3+2][0]*sfac_t[j*3+2][0]- sfac_t[i*3+2][1]*sfac_t[j*3+2][1]-    sfac[2*size_half+ik+m][0])/(itime);
-                            sfac[2*size_half+ik+m][1]+=(-sfac_t[i*3+2][0]*sfac_t[j*3+2][1]+sfac_t[i*3+2][1]*sfac_t[j*3+2][0]-     sfac[2*size_half+ik+m][1])/(itime);
-                            m++;
+                            sfac[ik+ii_tipi_matrix][0]+=       ( sfac_t[i*3][0]*  sfac_t[j*3][0]  -sfac_t[i*3][1]*  sfac_t[j*3][1] -sfac[ik+ii_tipi_matrix][0])/(itime);
+                            sfac[ik+ii_tipi_matrix][1]+=       (-sfac_t[i*3][0]*  sfac_t[j*3][1] + sfac_t[i*3][1]*  sfac_t[j*3][0] -sfac[ik+ii_tipi_matrix][1])/(itime);
+                            sfac[size_half  +ik+ii_tipi_matrix][0]+=  ( sfac_t[i*3+1][0]*sfac_t[j*3+1][0]-sfac_t[i*3+1][1]*sfac_t[j*3+1][1]  - sfac[  size_half+ik+ii_tipi_matrix][0])/(itime);
+                            sfac[size_half  +ik+ii_tipi_matrix][1]+=  (-sfac_t[i*3+1][0]*sfac_t[j*3+1][1]+sfac_t[i*3+1][1]*sfac_t[j*3+1][0] -  sfac[  size_half+ik+ii_tipi_matrix][1])/(itime);
+                            sfac[2*size_half+ik+ii_tipi_matrix][0]+=( sfac_t[i*3+2][0]*sfac_t[j*3+2][0]- sfac_t[i*3+2][1]*sfac_t[j*3+2][1]-    sfac[2*size_half+ik+ii_tipi_matrix][0])/(itime);
+                            sfac[2*size_half+ik+ii_tipi_matrix][1]+=(-sfac_t[i*3+2][0]*sfac_t[j*3+2][1]+sfac_t[i*3+2][1]*sfac_t[j*3+2][0]-     sfac[2*size_half+ik+ii_tipi_matrix][1])/(itime);
                         }
+                    }
                 }
-
+            ii_tipi += 1;  
             }
+
         }
+
+
+
+
+       // for (int kx=0;kx<nk;kx++){
+       //     unsigned int ikx=kx*nk*(nk/2+1)*(tipi_atomi+1)*tipi_atomi/2;
+       //     k[0]=PI/(box[1]-box[0])*kx;
+       //     for (int ky=0;ky<nk;ky++) {
+       //         unsigned int iky=ky*(nk/2+1)*(tipi_atomi+1)*tipi_atomi/2;
+       //         k[1]=PI/(box[1]-box[0])*ky;
+       //         for (int kz=0;kz<nk/2+1;kz++) {
+       //             for (unsigned int i=0;i<tipi_atomi*3;i++) {
+       //                 sfac_t[i][0]=0.0;
+       //                 sfac_t[i][1]=0.0;
+       //             }
+       //             unsigned int ik=ikx+iky+kz*(tipi_atomi+1)*tipi_atomi/2;
+       //             //k[2]=PI/(box[1]-box[0])*ky; ///// MERDA IL COPIA/INCOLLA
+       //             k[2]=PI/(box[1]-box[0])*kz;
+       //           //  sfac_t[0][0]=0.1 ;/////// TOGLIERE
+       //             s_fac_k(k,itimestep,sfac_t);
+       //             unsigned int m=0;
+       //             for (unsigned int i=0;i<tipi_atomi;i++)
+       //                 for (unsigned int j=0;j<=i;j++){
+       //                     //unsigned int m = (tipi_atomi+1)*tipi_atomi/2 - (i+1)*(i+2)/2 + j;
+
+       //                     sfac[ik+m][0]+=       ( sfac_t[i*3][0]*  sfac_t[j*3][0]  -sfac_t[i*3][1]*  sfac_t[j*3][1] -sfac[ik+m][0])/(itime);
+       //                     sfac[ik+m][1]+=       (-sfac_t[i*3][0]*  sfac_t[j*3][1] + sfac_t[i*3][1]*  sfac_t[j*3][0] -sfac[ik+m][1])/(itime);
+       //                     sfac[size_half  +ik+m][0]+=  ( sfac_t[i*3+1][0]*sfac_t[j*3+1][0]-sfac_t[i*3+1][1]*sfac_t[j*3+1][1]  - sfac[  size_half+ik+m][0])/(itime);
+       //                     sfac[size_half  +ik+m][1]+=  (-sfac_t[i*3+1][0]*sfac_t[j*3+1][1]+sfac_t[i*3+1][1]*sfac_t[j*3+1][0] -  sfac[  size_half+ik+m][1])/(itime);
+       //                     sfac[2*size_half+ik+m][0]+=( sfac_t[i*3+2][0]*sfac_t[j*3+2][0]- sfac_t[i*3+2][1]*sfac_t[j*3+2][1]-    sfac[2*size_half+ik+m][0])/(itime);
+       //                     sfac[2*size_half+ik+m][1]+=(-sfac_t[i*3+2][0]*sfac_t[j*3+2][1]+sfac_t[i*3+2][1]*sfac_t[j*3+2][0]-     sfac[2*size_half+ik+m][1])/(itime);
+       //                     m++;
+       //                 }
+       //             //std::cout<<" m=  "<<m<<std::endl ;
+       //         }
+
+       //     }
+       // }
 
 
 
@@ -153,15 +196,15 @@ CorrelatoreSpaziale & CorrelatoreSpaziale::operator =(const CorrelatoreSpaziale 
 
 double CorrelatoreSpaziale::corr(unsigned int rx, unsigned int ry, unsigned int rz, unsigned int itype,unsigned int idim) {
     if (itype >=tipi_atomi*(tipi_atomi+1)/2) {
-            std::cerr << "Errore: indice dei tipi fuori dal range in " __FILE__ "\n";
+            std::cerr << " Errore: indice dei tipi fuori dal range in " __FILE__ "\n";
             abort();
     }
     if (rx>=nk || ry>=nk || rz>=nk) {
-            std::cerr << "Errore: indice spaziale fuori dal range in " __FILE__ "\n";
+            std::cerr << " Errore: indice spaziale fuori dal range in " __FILE__ "\n";
             abort();
     }
     if (idim >= 3) {
-        std::cerr << "Errore: indice della coordinata fuori dal range in " __FILE__ "\n";
+        std::cerr << " Errore: indice della coordinata fuori dal range in " __FILE__ "\n";
     }
     return lista[idim*nk*nk*nk*tipi_atomi*(tipi_atomi+1)/2+ rx*nk*nk*tipi_atomi*(tipi_atomi+1)/2+
             ry*nk*tipi_atomi*(tipi_atomi+1)/2+
