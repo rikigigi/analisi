4d556ca7ca314efaf6f235eebdabd040ee6bc68d
Riccardo Bertossa <riccardo.bertossa@gmail.com>
2020-07-20 09:04:47 +0200

    ooops, a file was missing

---
+++ buffer_utils.h
@@ -0,0 +1,11 @@
+
+template <typename T>
+bool has_no_stride(const pybind11::buffer_info & buf) {
+    ssize_t stride=sizeof (T);
+    for (int i=buf.ndim-1; i>=0; --i) {
+        if (buf.strides[i]!=stride)
+            return false;
+        stride*=buf.shape[i];
+    }
+    return true;
+}
