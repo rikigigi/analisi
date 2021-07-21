e824f0593e807a54698b561cfa4dbc66c69922c0
Riccardo Bertossa <riccardo.bertossa@gmail.com>
2021-07-21 10:43:14 +0200

    test_cli shows diff

--- tests/test_cli.sh
+++ tests/test_cli.sh
@@ -31,6 +31,7 @@ TESTS=( "MSD_normal_full"   "-i $INPUT -Q"
 N_arr=${#TESTS[@]}
 N=$(( N_arr - 1 ))
 RETURN=0
+FLIST=""
 for i in $(seq 0 2 $N )
 do
     ip=$(( i + 1 ))
@@ -41,12 +42,15 @@ do
     if [ -f "$FTEST" ]
     then
         OUTPUT_F="`cat $FTEST`"
-         
-        if [ -z "`diff <(echo "$OUTPUT") <(echo "$OUTPUT_F")`" ]
+        DIFF="`diff <(echo "$OUTPUT") <(echo "$OUTPUT_F")`" 
+        if [ -z "$DIFF" ]
         then
             echo SUCCESS $NAME
         else
             echo FAILED $NAME
+            echo "$DIFF"
+            FLIST="$FLIST
+FAILED $NAME"
             RETURN=$(( RETURN + 1 ))
         fi
     else
@@ -55,4 +59,5 @@ do
     fi
 
 done
+echo "$FLIST"
 exit $RETURN
