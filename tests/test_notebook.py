0c5fafb2f4286a42bcd51597cde23a284f40da9c
Riccardo Bertossa <rbertoss@sissa.it>
2022-06-10 11:27:46 +0200

    removed notebook test for windows

--- tests/test_notebook.py
+++ tests/test_notebook.py
@@ -5,10 +5,17 @@ from testbook import testbook
 
 @pytest.fixture(scope='module')
 def tb():
+    import pyanalisi as pa
+    if not pa.has_mmap():
+        yield None
     with testbook('../notebooks/calc_inspector.ipynb', execute=True) as tb:
         yield tb
 
 def test_density(tb,num_regression):
+    import pyanalisi as pa
+    if not pa.has_mmap():
+        warnings.warn(UserWarning('NOT TESTING NOTEBOOK. This compiled version of pyanalisi DOES NOT SUPPORT READING LAMMPS FILES.'))
+        return
     tb.inject("_cell=density[1].flatten().tolist()")
     tb.inject("_dens=density[0].flatten().tolist()")
     density = tb.ref("_dens")
@@ -19,6 +26,10 @@ def test_density(tb,num_regression):
     
 
 def disabledtest_sh(tb,num_regression):
+    import pyanalisi as pa
+    if not pa.has_mmap():
+        warnings.warn(UserWarning('NOT TESTING NOTEBOOK. This compiled version of pyanalisi DOES NOT SUPPORT READING LAMMPS FILES.'))
+        return
     tb.inject("_shfit=np.array(shp[3]).tolist()")
     shfit = tb.ref("_shfit")
     num_regression.check({
@@ -26,6 +37,10 @@ def disabledtest_sh(tb,num_regression):
              })
 
 def test_gofr(tb,num_regression):
+    import pyanalisi as pa
+    if not pa.has_mmap():
+        warnings.warn(UserWarning('NOT TESTING NOTEBOOK. This compiled version of pyanalisi DOES NOT SUPPORT READING LAMMPS FILES.'))
+        return
     tb.inject("_gofr=gofr.tolist()")
     gofr=tb.ref("_gofr")
     num_regression.check({'gofr':np.array(gofr).flatten()})
