7244c8fa541193d4aa2a280d6dc42e26a11272a4
Riccardo Bertossa <rbertoss@materys.com>
2023-11-01 22:26:15 +0000

    added ipykernel to dependencies
    https://github.com/jupyter-widgets/ipywidgets/issues/3731

--- install/install_test.sh
+++ install/install_test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -e
-pip install pytest pytest-regressions pandas matplotlib numpy scipy testbook k3d
+pip install pytest pytest-regressions pandas matplotlib numpy scipy testbook k3d ipykernel
